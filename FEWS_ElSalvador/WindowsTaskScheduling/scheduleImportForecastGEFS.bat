fa9a55d1b660422bd8baa079c0bfa04f256f8514
rodolfo <roalva82@gmail.com>
2022-04-25 09:25:48 -0600

    incluye una segunda cuenca en el reporte básico de lluvias

---
+++ FEWS_ElSalvador/WindowsTaskScheduling/scheduleImportForecastGEFS.bat
@@ -0,0 +1,6 @@
+# %1: path to region home
+# %2: task properties file
+ 
+cd c:\Users\Rodolfo\Desktop\Consultancy\MARN\repository\FEWS_ElSalvador
+
+c:\Users\Rodolfo\Desktop\Consultancy\MARN\repository\FEWS_ElSalvador\FEWS_ElSalvador.lnk "runTask=c:\Users\Rodolfo\Desktop\Consultancy\MARN\repository\FEWS_ElSalvador\WindowsTaskScheduling\Task_ImportForecastGEFS.xml"
