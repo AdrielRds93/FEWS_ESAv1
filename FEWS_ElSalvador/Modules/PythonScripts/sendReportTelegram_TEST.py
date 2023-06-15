cbcc07be93d3665a78a241930cc3d9f9b5d98626
rodolfo <roalva82@gmail.com>
2023-06-15 16:45:43 -0400

    cambio a script telegram

--- FEWS_ElSalvador/Modules/PythonScripts/sendReportTelegram_TEST.py
+++ FEWS_ElSalvador/Modules/PythonScripts/sendReportTelegram_TEST.py
@@ -1,10 +1,15 @@
 import sys
+import asyncio
 import telegram
 
-bot = telegram.Bot(token='5218957595:AAFabMP37uZZtYSNuf_3UPyDxWQkZFAZw98')
-chat_id = -1001510346693
-filename = sys.argv[1]
-caption = sys.argv[2]
+async def main():
+	bot = telegram.Bot(token='5218957595:AAFabMP37uZZtYSNuf_3UPyDxWQkZFAZw98')
+	chat_id = -1001510346693
+	filename = sys.argv[1]
+	caption = sys.argv[2]
 
-bot.send_photo(chat_id=chat_id, photo=open(filename, 'rb'), caption=caption)
-await client.run_until_disconnected()
+	async with bot:
+		await bot.send_photo(chat_id=chat_id, photo=open(filename, 'rb'), caption=caption)
+
+if __name__ == '__main__':
+    asyncio.run(main())
